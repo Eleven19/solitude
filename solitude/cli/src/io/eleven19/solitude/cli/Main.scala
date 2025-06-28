baca16d34a3302f2883662264551a4bdbbb356cf
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2025-06-28 06:20:15 -0500

    Initial commit... setting things up

---
+++ solitude/cli/src/io/eleven19/solitude/cli/Main.scala
@@ -0,0 +1,5 @@
+package io.eleven19.solitude.cli 
+
+object Main extends App:
+  scribe.info("Starting Solitude CLI...")
+  println("Welcome to Solitude CLI!")
---
+++ solitude/hosting/src/io/eleven19/solitude/hosting/Host.scala
@@ -0,0 +1,3 @@
+package io.eleven19.solitude.hosting
+
+final case class Host()
