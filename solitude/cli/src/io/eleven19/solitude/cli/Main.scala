565ca103069ffc5f41ae2f835571cc8ff96694e7
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2025-06-28 21:33:48 -0500

    Ensure Python integration works

--- solitude/cli/src/io/eleven19/solitude/cli/Main.scala
+++ solitude/cli/src/io/eleven19/solitude/cli/Main.scala
@@ -1,5 +1,12 @@
 package io.eleven19.solitude.cli 
+import org.graalvm.polyglot.Context
+import scala.util.Using
 
 object Main extends App:
   scribe.info("Starting Solitude CLI...")
   println("Welcome to Solitude CLI!")
+  Using(Context.create()){context => 
+    context.eval("python", "print('Hello from GraalPy!')")
+  }
+end Main
+  
